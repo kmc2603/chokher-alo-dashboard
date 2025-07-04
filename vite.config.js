333be2edb40d5f4bef2a18a6cd9dff6f7c456428
kmc2603 <kumar.chowdhury10@gmail.com>
2025-07-04 19:34:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // Make sure it points to current directory
   plugins: [react()],
 });
